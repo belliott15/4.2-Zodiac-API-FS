1079002168a35a6e787a43a62c049b9e7c49193e
Beau Elliott <belliott1516@gmail.com>
2022-06-06 15:47:07 -0700

    completed tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,10 +2,28 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const { zodiacData } = require('../data/zodiac-data');
 
 describe('4.2-Zodiac-API-FS routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('/zodiac should return a list of all zodiacs without ID', async () => {
+    const res = await request(app).get('/zodiac');
+    const expected = zodiacData.map(({ name, dates, symbol }) => ({ name, dates, symbol }));
+    expect(res.body).toEqual(expected);  
+  });
+
+  it('/zodiac/:id should return one zodiac', async () => {
+    const res = await request(app).get('/zodiac/2');
+    const zodiac2 = { 
+      id:'2', 
+      name:'aries', 
+      dates:'Mar 21 - Apr 19', 
+      symbol:'Ram'
+    };
+    expect(res.body).toEqual(zodiac2);
   });
 
   afterAll(() => {
